f143ef7441419c7438bc4b8c31fb44fa87582bc4
JustAlexOne <alex.cherevatiy@gmail.com>
2018-12-02 19:19:53 +0200

    Add some more features

--- src/main/java/com/justalex/pnp/GroovyGrabber.groovy
+++ src/main/java/com/justalex/pnp/GroovyGrabber.groovy
@@ -19,12 +19,42 @@ class GroovyGrabber {
 
 
     static void main(String[] args) {
-        def dataFileJson = PnpWorker.readDataFile("src/main/resources/dataFile.json")
+        def dataFileJson = PnpWorker.readDataFile("src/main/resources/dataFile_unescaped.json")
 //        def dataFileJson = PnpWorker.readDataFile("src/main/resources/a_10_cards.json")
+
         def cards = dataFileJson.en.cards.byId
+        println("Cards total: ${cards.size()}")
+
+        ImageWorker imageWorker = new ImageWorker()
+        cards.each {it ->
+            def value = it.getValue()
+            def id = value.id
+            println(id)
+            String imageUrl = URLDecoder.decode(value.image_url, "UTF-8")
+            String image_filename = imageUrl.split("/")[-1]
+            println("URL: $imageUrl")
+            println("imageFileName: $image_filename")
+            BufferedImage imageFromUrl = imageWorker.getImageFromUrl(imageUrl)
+            imageWorker.saveImage(imageFromUrl, "card_images/all/$image_filename")
+//            def imageFilename = value.image_filename
+//            if (imageFilename != null) {
+//                assert image_filename == imageFilename
+//            }
+//            if (id == "L60") {
+//                return true
+//            } else {
+//                String filename = value.image_filename
+//                def image_url = value.image_url
+////                def image_url = URLDecoder.decode(value.image_url, "UTF-8")
+//                assert image_url.contains(filename)
+//            }
+//            return false
+        }
+        println("parsed")
+//        println("Filenames size: ${fileNamesList.size()}")
+//        fileNamesList.each {println(it)}
 //        def warbands = dataFileJson.en.warbands.byId
 //        println("Warbands total: ${warbands.size()}")
-        println("Cards total: ${cards.size()}")
 //        warbands.each {println(it)}
 
 //       parsedJson def list = ["a", "b", "c"]
@@ -40,16 +70,27 @@ class GroovyGrabber {
 //        println(dataFileJson.en.cards.byId.S001) // works!!!
 //        println(cards["S007"].name)
         // todo unescape Java and work with \x...
-        println(cards.S001.image_url)
-        String str = cards.S001.image_url
-        println("Str: $str")
+//        println(cards.S001.image_url)
+//        String str = cards.S001.image_url
 
 //        def imageWorker = new ImageWorker()
-//        BufferedImage imageFromUrl = imageWorker.getImageFromUrl(str)
-//        imageWorker.saveImage(imageFromUrl, "src/main/resources/m_img.png")
+//        println(cards)
+//        def counter = 1;
+//        cards.entrySet().each {
+//            def card = it.getValue()
+////            String fileName = card.image_filename
+//            String fileName = "src/main/resources/imgs/${counter++}.png"
+//            String cardName = card.name
+//            String url = card.image_url
+//            println("Card: $cardName")
+//            println("Filename: $fileName")
+//            println("Url: $url")
+//            BufferedImage imageFromUrl = imageWorker.getImageFromUrl(url)
+//            imageWorker.saveImage(imageFromUrl, fileName)
+//        }
 
 
-        def bytes = Unirest.get(str).asBinary().getBody().bytes
+        /*def bytes = Unirest.get(str).asBinary().getBody().bytes
         Document document = new Document();
         PdfWriter.getInstance(document, new FileOutputStream("iTextImageExample.pdf"));
         document.open();
@@ -60,7 +101,7 @@ class GroovyGrabber {
         document.add(img2);
 
         document.close();
-
+*/
         println("Done!")
     }
 
--- src/main/java/com/justalex/pnp/PdfWorker.groovy
+++ src/main/java/com/justalex/pnp/PdfWorker.groovy
@@ -1,24 +1,123 @@
 package com.justalex.pnp
 
+import com.itextpdf.text.BadElementException
+import com.itextpdf.text.BaseColor
+import com.itextpdf.text.Chunk
 import com.itextpdf.text.Document
+import com.itextpdf.text.Element
+import com.itextpdf.text.Font
+import com.itextpdf.text.FontFactory
 import com.itextpdf.text.Image
+import com.itextpdf.text.PageSize
+import com.itextpdf.text.Phrase
+import com.itextpdf.text.pdf.PdfPCell
+import com.itextpdf.text.pdf.PdfPTable
 import com.itextpdf.text.pdf.PdfWriter
+import groovy.io.FileType
 
+import java.nio.file.Path
 import java.nio.file.Paths
+import java.util.stream.Stream
 
 
 class PdfWorker {
 
-//    public static void insertImagesIntoPdf(some list) {
-//        Path path = Paths.get(ClassLoader.getSystemResource("Java_logo.png").toURI());
-//
+    static void main(String[] args) {
 //        Document document = new Document();
-//        PdfWriter.getInstance(document, new FileOutputStream("iTextImageExample.pdf"));
-//        document.open();
-//        Image img = Image.getInstance(path.toAbsolutePath().toString());
-//        document.add(img);
-//
-//        document.close();
-//    }
+        Document document = new Document(PageSize.A4, 15, 15, 15, 15);
+        PdfWriter.getInstance(document, new FileOutputStream("myFile1.pdf"));
+
+        document.open();
+
+//        Font font = FontFactory.getFont(FontFactory.COURIER, 25, BaseColor.BLACK);
+//        Chunk chunk = new Chunk("Hello World", font);
+//        document.add(chunk);
+
+        def imageFiles = []
+
+        def dir = new File("card_images/all")
+        dir.eachFileRecurse (FileType.FILES) { file ->
+            imageFiles << file
+        }
+        println(imageFiles.size())
+//        list.each {println(it)}
+
+//        1.upto(10){ imageFiles.add("src/main/resources/imgs/${it}.png")}
+        imageFiles = imageFiles.subList(0, 9)
+        // 550x770
+
+        // real card size = 63.5 x 88 mm
+
+        // user units: 178.58268 x 249.44904
+        PdfPTable table = new PdfPTable(3);
+//        table.setWidthPercentage(98)
+        float resWidth = 178.58268f * 3f
+        println("resWidth: $resWidth")
+        table.setTotalWidth(resWidth);
+        table.setLockedWidth(true);
+//        table.setSpacingBefore(0f);
+//        table.setSpacingAfter(0f);
+
+        imageFiles.each {
+            Image image = Image.getInstance(it.getAbsolutePath())
+//            image.scalePercent(30)
+            PdfPCell cell = new PdfPCell()
+            cell.setPadding(0f)
+//            cell.setFixedHeight(249.44904f)
+            cell.setImage(image)
+
+//            table.addCell(image)
+            table.addCell(cell)
+        }
+        int extraCells = (imageFiles.size() % 3)
+        extraCells = extraCells == 0 ? 0 : 3 - extraCells
+        println("Adding $extraCells extra cells")
+        extraCells.times {
+            table.addCell("")
+        }
+        document.add(table);
+        document.close();
+    }
+
+    private static void addTableHeader(PdfPTable table) {
+       ["column header 1", "column header 2", "column header 3"].each { columnTitle ->
+            PdfPCell header = new PdfPCell();
+            header.setBackgroundColor(BaseColor.LIGHT_GRAY);
+            header.setBorderWidth(2);
+            header.setPhrase(new Phrase(columnTitle));
+            table.addCell(header);
+        }
+    }
+
+    private static void addRows(PdfPTable table) {
+        table.addCell("row 1, col 1");
+        table.addCell("row 1, col 2");
+        table.addCell("row 1, col 3");
+    }
+
+    private static void addCustomRows(PdfPTable table)
+            throws URISyntaxException, BadElementException, IOException {
+//        Path path = Paths.get(new URI("/Users/justalex/Projects/wh_grab/src/main/java/com/justalex/imageCleanup/001-NV_ENG.png"));
+//        Path path = Paths.get(ClassLoader.getSystemResource("Java_logo.png").toURI());
+        Image img = Image.getInstance("/Users/justalex/Projects/wh_grab/src/main/java/com/justalex/imageCleanup/001-NV_ENG.png");
+//        img.scalePercent((88 / img.getPlainHeight() * 100) as Float, (63.5 / img.getPlainWidth() * 100) as Float)
+        img.scalePercent((88 / img.getPlainHeight() * 100) as Float)
+//        img.scaleAbsolute(100, 100)
+
+
+        println("height: ${img.getPlainHeight()}")
+        println("width: ${img.getPlainWidth()}")
+
+        PdfPCell imageCell = new PdfPCell(img);
+        table.addCell(imageCell);
+
+        PdfPCell horizontalAlignCell = new PdfPCell(new Phrase("row 2, col 2"));
+        horizontalAlignCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.addCell(horizontalAlignCell);
+
+        PdfPCell verticalAlignCell = new PdfPCell(new Phrase("row 2, col 3"));
+        verticalAlignCell.setVerticalAlignment(Element.ALIGN_BOTTOM);
+        table.addCell(verticalAlignCell);
+    }
 
 }
